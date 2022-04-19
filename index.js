52ad0f4be96edf6f54486280c2c8969d70c974c6
tnahira <101633448+tnahira@users.noreply.github.com>
2022-04-19 11:06:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let section = document.querySelectorAll('section');
-
 window.onscroll = () =>{
 
     if(window.scrollY > 60){
@@ -8,23 +6,9 @@ window.onscroll = () =>{
     else{
         document.querySelector('#scroll-top').classList.remove('active');
     }
-
-    section.forEach(sec =>{
-        let top = window.scrollY;
-        let height = sec.offsetHeight;
-        let offset = sec.offsetTop - 150;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-            });
-        };
-    });
 }
 
-  const swiper = new Swiper(".review-slider", {
+ var swiper = new Swiper("swiper", {
     spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
